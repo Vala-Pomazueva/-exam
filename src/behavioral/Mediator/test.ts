c13c5e98222bd519500569883ceb7e388b9defc6
Vala-Pomazueva <valapomazueva@gmail.com>
2026-05-21 12:55:42 +0300

    Update test.ts

--- src/behavioral/Mediator/test.ts
+++ src/behavioral/Mediator/test.ts
@@ -1,1 +1,27 @@
+class ChatRoom {
+  showMessage(user: User, message: string) {
+    const time = new Date().toLocaleTimeString();
+    console.log(`[${time}] ${user.getName()}: ${message}`);
+  }
+}
 
+class User {
+  constructor(private name: string, private chat: ChatRoom) {}
+
+  getName() {
+    return this.name;
+  }
+
+  send(message: string) {
+    this.chat.showMessage(this, message);
+  }
+}
+
+// DEMO
+const room = new ChatRoom();
+
+const user1 = new User("Alex", room);
+const user2 = new User("Bob", room);
+
+user1.send("Hi Bob!");
+user2.send("Hello Alex!");
